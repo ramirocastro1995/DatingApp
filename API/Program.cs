0542b67481d85f150876dcc8a6318ebec0e553bb
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-01-22 11:41:44 -0300

    End of Section 7

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,4 @@
-using System.Text;
 using API;
-using API.Data;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,7 +9,9 @@ var app = builder.Build();
 
 //app.UseHttpsRedirection();
 
+
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 //Do you have a token?
 app.UseAuthentication();
