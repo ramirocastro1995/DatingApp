e5b9c46424f1ee65e3d838956f9be2708fd909d5
Ramiro Castro <ramirosalvadorcastro95@hotmail.com>
2024-02-01 16:18:45 -0300

    End of section 9

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,19 @@
+import { Photo } from "./Photo";
+
+export interface Member {
+    id: number;
+    userName: string;
+    photoUrl: string;
+    age: number;
+    knowAs: string;
+    created: string;
+    lastActive: string;
+    gender: string;
+    introduction: string;
+    lookingFor: string;
+    interests: string;
+    city: string;
+    country: string;
+    photos: Photo[];
+  }
+  
